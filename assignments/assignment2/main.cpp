e3cbe6564bf5fc02124af18b6f241fb1cd15ea99
River Ranz <river.ranz@mymail.champlain.edu>
2025-02-04 09:14:32 -0500

    created depth buffers & textures, started with depth shader

--- assignments/assignment2/main.cpp
+++ assignments/assignment2/main.cpp
@@ -77,9 +77,19 @@ int main() {
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 
+	// orthographic projection light source
+	float nearPlane = 1.0f, farPlane = 7.5f;
+	glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, nearPlane, farPlane);
+
+	// view matrix
+	glm::mat4 lightView = glm::lookAt(glm::vec3(-2.0f, 4.0f, -1.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	// light space transformation matrix
+	glm::mat4 lightSpaceMatrix = lightProjection * lightView;
 
 	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
 	ew::Shader screenShader = ew::Shader("assets/screen.vert", "assets/screen.frag");
+	ew::Shader depthShader = ew::Shader("assets/depthShader.vert", "assets/depthShader.frag");
 
 	ew::Model monkeyModel = ew::Model("assets/suzanne.fbx");
 	ew::Transform monkeyTransform;
@@ -117,6 +127,30 @@ int main() {
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+	// depth map
+	unsigned int depthMapFBO;
+	glGenFramebuffers(1, &depthMapFBO);
+
+	// 2D texture (using depth buffer)
+	const unsigned int SHADOW_WIDTH = 1080, SHADOW_HEIGHT = 720;
+
+	unsigned int depthMap;
+	glGenTextures(1, &depthMap);
+	glBindTexture(GL_TEXTURE_2D, depthMap);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// attach depth texture to depth buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+	// no color buffer
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 	glEnable(GL_CULL_FACE);
 	// back face culling
 	glCullFace(GL_BACK);
@@ -135,13 +169,25 @@ int main() {
 		cameraController.move(window, &camera, deltaTime);
 
 		// first pass
+		// render to depth map
+		depthShader.use();
+		depthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
 		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+		// render with shadow mapping
+		glViewport(0, 0, screenWidth, screenHeight);
 		glClearColor(0.6f, 0.8f, 0.92f, 1.0f); // background color
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
 		glEnable(GL_DEPTH_TEST);
 
 		glBindTextureUnit(0, texture);
 		glBindTextureUnit(1, normalMap);
+		glBindTextureUnit(2, depthMap);
 
 		shader.use();
 		shader.setInt("_MainTex", 0);
@@ -215,6 +261,16 @@ void drawUI() {
 	{
 		resetCamera(&camera, &cameraController);
 	}
+	ImGui::End();
+
+	ImGui::Begin("Shadow Map");
+	//child allows to fill all of the window
+	ImGui::BeginChild("Shadow Map");
+	// stretch image
+	ImVec2 windowSize = ImGui::GetWindowSize();
+	// invert 0-1 V to flip vertically
+	ImGui::Image((ImTextureID)3, windowSize, ImVec2(0, 1), ImVec2(1, 0));
+	ImGui::EndChild();
 	ImGui::End();
 
 	ImGui::Render();
